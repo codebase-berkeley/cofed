f6b6b7759bbeb20033c664dbde9c098c1b1cf99d
Eric Hao <ehao@berkeley.edu>
2020-10-18 01:34:40 -0400

    removed lint errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import React from 'react';
->>>>>>> 2b39a64... typecheckig in CardComponent and Tag
 import './App.css';
 import Login from './Login';
 import React from 'react';
@@ -11,25 +7,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-// export default App;
