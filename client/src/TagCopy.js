04b3f1f8dffb08c32da52685de46af3070dcf6d0
Jane Lee <jane-lee@berkeley.edu>
2020-10-21 23:39:12 -0700

    completed edit toggle

---
+++ client/src/TagCopy.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TagCopy.css';
+
+export default function Tag(props) {
+  const edit = props.edit;
+  const [existance, setExistance] = React.useState(true);
+
+  function renderDisplay() {
+    return (
+      <div className="tagShape" edit={props.edit}>
+        {props.text}
+      </div>
+    );
+  }
+
+  function renderEdit() {
+    return (
+      <div
+        onClick={() => props.onDelete(props.index)}
+        className="tagShapeEdit"
+        edit={props.edit}
+      >
+        {'ⓧ  ' + props.text}
+      </div>
+    );
+  }
+
+  return <>{edit ? renderEdit() : renderDisplay()}</>;
+
+  // return (
+  //   <div
+  //   onClick={() => props.onDelete(props.index)}
+  //   className="tagShape"
+  //   edit={props.edit}
+  //   >
+  //   {"ⓧ " + props.text}
+  //   </div>
+  // )
+}
+
+Tag.propTypes = { text: PropTypes.string };
