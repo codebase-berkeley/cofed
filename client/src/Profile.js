dd7da34c2affed687e99962357ea208d0d776533
Jane Lee <jane-lee@berkeley.edu>
2020-10-21 23:39:12 -0700

    completed edit toggle

--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,18 +1,160 @@
 import React from 'react';
 import './Profile.css';
-import Tag from './Tag.js';
+import Tag from './TagCopy.js';
 import cofedlogo from './assets/CoFEDlogo.png'
 import instagram from './assets/instagram.svg'
 import facebook from './assets/facebook.svg'
 import tina from './assets/tina.png'
 
 export default function Profile(props) {
+    const [edit, setEdit] = React.useState(false);
+    const [name, setName] = React.useState(props.name); //from props
+    const [location, setLocation] = React.useState(props.location); //from props
+
+    const [profilePic, setProfilePic] = React.useState(props.profilePic);
+    const [phone, setPhone] = React.useState(props.phone);
+    const [tags, setTags] = React.useState(props.tags);
+    const [mission, setMission] = React.useState(props.missionText);
+    const [description, setDescription] = React.useState(props.descText);
+    const [instaLink, setInstaLink] = React.useState(props.instaLink);
+    const [fbLink, setFbLink] = React.useState(props.fbLink);
+
+    const [website, setWebsite] = React.useState(props.website);
+    const [websiteLink, setWebsiteLink] = React.useState(props.websiteLink);
+    const [email, setEmail] = React.useState(props.email); 
+    const [emailLink, setEmailLink] = React.useState(props.emailLink);
+    
+    function handleDelete(tagIndex) {
+        const newTags = tags.filter((tag,i) => i != tagIndex);
+        setTags(newTags);
+      };
+
     function toggleEdit() {
         //edit button function
+        setEdit(!edit);
     }
 
-    return (
-        <div className = "blueBorder">
+    function renderEdit() {
+        return(
+            <div className = "blueBorder">
+                <div className="topDiv">
+                    <div className="logoDiv">
+                        <a href="https://www.cofed.coop/"><img src={cofedlogo} className="logo"/></a>
+                    </div>
+                    <div className="editDiv">
+                        <button 
+                            className="saveButton" 
+                            onClick={toggleEdit}>Save</button>
+                    </div>
+                </div>
+                <div className="ProfileTextTagsContainer">
+                    <div className="picTextContainer">
+                        <div className="profPicContainer">
+                            <button className="editPic">+</button> 
+                        </div>
+                        <div className="editProfileTextContainer">
+                            <input 
+                                className = "NameInput"
+                                type="text"
+                                placeholder="Enter name:"
+                                value={name}
+                                onChange={e => setName(e.target.value)}>
+                            </input>
+
+                            <input 
+                                className = "smallInput"
+                                type="text"
+                                placeholder="Enter location:"
+                                value={location}
+                                onChange={e => setLocation(e.target.value)}>
+                            </input>
+
+                            <input 
+                                className = "smallInput"
+                                type="text"
+                                placeholder="Enter website link:"
+                                value={website}
+                                onChange={e => setWebsite(e.target.value)}>
+                            </input>
+
+                            <input 
+                                className = "smallInput"
+                                type="text"
+                                placeholder="Enter email address:"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}>
+                            </input>
+
+                            <input 
+                                className = "smallInput"
+                                type="text"
+                                placeholder="Enter phone number:"
+                                value={phone}
+                                onChange={e => setPhone(e.target.value)}>
+                            </input>
+                        </div>
+                    </div>  
+
+                    <div className="tagsContainer">
+                    {tags &&
+                        tags.map((text, index) => 
+                        <Tag 
+                        key={index}
+                        text={text}
+                        index={index}
+                        edit={edit}
+                        onDelete={handleDelete}
+                        />)}
+                    </div>
+
+                </div>
+                <div className="descriptions"> 
+                    <hr/>
+                    <div className = "header">Mission Statement</div>
+                    <textarea 
+                        className = "missionInput"
+                        type="text"
+                        placeholder="Enter mission statement:"
+                        maxLength = "350"
+                        value={mission}
+                        onChange={e => setMission(e.target.value)}>
+                    </textarea>
+                    <hr/>       
+                    <div className = "header">Description</div>
+                    <textarea 
+                        className = "descriptionInput"
+                        type="text"
+                        placeholder="Enter description:"
+                        maxLength = "1000"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}>
+                    </textarea>
+                    <hr/>
+                </div>
+                <div className="socialsDiv">
+                    <input 
+                        className = "smallInput"
+                        type="text"
+                        placeholder="Enter facebok link:"
+                        value={fbLink}
+                        onChange={e => setFbLink(e.target.value)}>
+                    </input>
+                    <input 
+                        className = "smallInput"
+                        type="text"
+                        placeholder="Enter instagram link:"
+                        value={instaLink}
+                        onChange={e => setInstaLink(e.target.value)}>
+                    </input>
+                </div>
+            </div>
+        )
+
+    }
+
+    function renderDisplay() {
+        return(
+            <div className = "blueBorder">
             <div className="topDiv">
                 <div className="logoDiv">
                     <a href="https://www.cofed.coop/"><img src={cofedlogo} className="logo"/></a>
@@ -23,33 +165,49 @@ export default function Profile(props) {
             </div>
             <div className="ProfileTextTagsContainer">
                 <div className="picTextContainer">
-                    <img className="profilePic" alt="Image" src={tina} />
+                    <img className="profilePic" alt="Image" src={tina}/>
                     <div className="profileTextContainer">
-                        <b className="CoOpName">{props.name}</b>
-                        <div className="CoOpLocation">{props.location}</div>
-                        <a className="CoOpContact" href={props.websiteLink} target="_blank">{props.website}</a>
-                        <a className="CoOpContact" href={props.emailLink} target="_blank">{props.email}</a>
-                       <div className="CoOpContact">{props.phone} </div>
+                        <b className="CoOpName">{name}</b>
+                        <div className="CoOpLocation">{location}</div>
+                        <a className="CoOpContact" href={websiteLink} target="_blank">{website}</a>
+                        <a className="CoOpContact" href={emailLink} target="_blank">{email}</a>
+                        <div className="CoOpContact">{phone} </div>
                     </div>
                 </div>  
                 <div className="tagsContainer">
-                {props.tags &&
-                    props.tags.map(text => <Tag text={text} key={props.name + text} />)}
-                </div>
+                    {tags &&
+                        tags.map((text, index) => 
+                        <Tag 
+                        key={index}
+                        text={text}
+                        index={index}
+                        edit={edit}
+                        onDelete={handleDelete}
+                        />)}
+                    </div>
             </div>
             <div className="descriptions"> 
                 <hr/>
                 <div className = "header" >Mission Statement</div>
-                <div className = "profileinfo">{props.missionText} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                <div className = "profileinfo">{mission}</div>
                 <hr/>       
                 <div className = "header">Description</div>
-                <div className = "profileinfo">{props.descText} Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</div>
+                <div className = "profileinfo">{description}</div>
                 <hr/>
             </div>
             <div className="socialsDiv">
-                <a href="https://facebook.com" target="_blank"><img src={facebook} className="socialButton"/></a>
-                <a href="https://instagram.com" target="_blank"><img src={instagram} className="socialButton"/></a>
+                <a href={instaLink} target="_blank"><img src={facebook} className="socialButton"/></a>
+                <a href={fbLink} target="_blank"><img src={instagram} className="socialButton"/></a>
             </div>
-        </div>    
+        </div>
+    );  
+    
+
+    }
+    
+    return (
+        <>
+            {edit ? renderEdit() : renderDisplay()}
+        </>
     )
 }
