a7a3204050a9293a535d56c353d7c24c7ce7cd7c
Eric Hao <ehao@berkeley.edu>
2020-11-09 01:15:03 -0500

    COPS stuff added

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,13 +3,10 @@ const db = require('../../db/index');
 const router = express.Router();
 
 // This is an example SQL query - use this as a template
-router.get('/', async (req, res) => {
+router.get('/profile', async (req, res) => {
   try {
-    console.log('hello!');
     const query = await db.query(`SELECT 'this is dummy text' as text;`);
-    // res.send(query);
-    // res.send('hello!');
-    res.send(query.rows[0]['text']);
+    res.send(query.rows[0]);
   } catch (error) {
     console.log(error.stack);
   }
