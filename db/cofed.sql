a1e6fe8216d3d2841d5b6814f34f0164ff9702aa
Eric Hao <ehao@berkeley.edu>
2020-12-09 23:22:08 -0500

    added a new category and deleted the dummy data in the database

--- db/cofed.sql
+++ db/cofed.sql
@@ -58,31 +58,13 @@ GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO root;
 
 GRANT ALL PRIVILEGES ON DATABASE cofed TO root;
 
-INSERT INTO coops
-    (email, hashed_pass, coop_name, phone_number, addr, latitude, longitude, website, mission_statement, description_text, profile_pic, insta_link, fb_link)
-VALUES
-    ('test@gmail.com', 'password', 'Test', '123456789',
-        'Berkeley, CA', 37.8715, -123.2730, 'test.com', 'test', 'test', 'test', 'test',
-        'test'),
-    ('test2@gmail.com', 'password2', 'Test2', '123456789',
-        'Berkeley, CA2', 37.8715, -123.2730, 'test.com', 'test2', 'test2', 'test2', 'test2',
-        'test2'),
-    ('test3@gmail.com', 'password3', 'Test3', '123456789',
-        'Berkeley, CA3', 37.8715, -123.2730, 'test.com', 'test3', 'test3', 'test3', 'test3',
-        'test3'),
-    ('test4@gmail.com', 'password4', 'Test4', '123456789',
-        'Berkeley, CA2', 37.8715, -123.2730, 'test.com', 'test2', 'test2', 'test2', 'test2',
-        'test2'),
-    ('test5@gmail.com', 'password5', 'Test5', '123456789',
-        'Berkeley, CA3', 37.8715, -123.2730, 'test.com', 'test3', 'test3', 'test3', 'test3',
-        'test3');
-
 INSERT INTO categories
     (category_name)
 VALUES
     ('Organization Structure'),
     ('Products'),
-    ('Coop Size');
+    ('Coop Size'),
+    ('Services');
 
 INSERT INTO tags
     (tag_name, category_id)
@@ -98,32 +80,12 @@ VALUES
     ('Spices' , 2),
     ('1-10', 3),
     ('10-50', 3),
-    ('50-100', 3);
-
-
-INSERT INTO coop_tags
-    (coop_id, tag_id)
-VALUES
-    (1, 1),
-    (1, 2),
-    (1, 3),
-    (1, 6),
-    (1, 7),
-    (1, 12),
-    (2, 1),
-    (2, 4),
-    (2, 5),
-    (3, 2),
-    (3, 3),
-    (3, 7),
-    (4, 2),
-    (4, 3),
-    (4, 7),
-    (4, 11),
-    (5, 2),
-    (5, 4),
-    (5, 8),
-    (5, 10);
-
+    ('50-100', 3),
+    ('100+', 3),
+    ('Advocacy', 4),
+    ('Education', 4),
+    ('Food Services', 4),
+    ('Purchasing and Acquisition', 4);
+    
 
     
